7886ba7c8d6016611fb30f6bb804ff8fbcd21931
KNR1997 <kethakaranasinghe@gmail.com>
2025-10-14 21:08:29 +0530

    refactor: Type views

--- product/views/type.py
+++ product/views/type.py
@@ -1,6 +1,3 @@
-from rest_framework import status
-from rest_framework.response import Response
-
 from core.views.base import BaseViewSet
 from product.models import Type
 from product.serializers import TypeSerializer
@@ -11,66 +8,24 @@ class TypeViewSet(BaseViewSet):
     model = Type
     serializer_class = TypeSerializer
     search_fields = ["name", "slug"]
-    filterset_fields = ["type__id"]
+    filterset_fields = []
 
     def get_queryset(self):
-        # Add your prefetch/select optimizations
         return (
-            super()
-            .get_queryset()
-            .select_related("type")
-            .prefetch_related("categories", "tags")
+            self.filter_queryset(super().get_queryset())
         )
 
     def list(self, request, *args, **kwargs):
-        # Get filtered queryset
-        queryset = self.get_queryset()
+        return super().list(request, *args, **kwargs)
 
-        # Apply pagination if set in settings.py
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.serializer_class(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        # If pagination is not enabled
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def retrieve(self, request, pk):
-        type = (
-            self.get_queryset()
-            .filter(pk=pk)
-        ).first()
-
-        if type is None:
-            return Response(
-                {"error": "Type does not exist"}, status=status.HTTP_404_NOT_FOUND
-            )
-
-        serializer = self.serializer_class(Type)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
-        serializer = TypeSerializer(
-            data={**request.data}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return super().create(request, *args, **kwargs)
 
-    def partial_update(self, request, pk, *args, **kwargs):
-        type = Type.objects.get(pk=pk)
-        serializer = TypeSerializer(
-            type,
-            data={**request.data},
-            partial=True
-        )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk, *args, **kwargs):
-        type = Type.objects.get(pk=pk)
-        type.delete()
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
