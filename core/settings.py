2531feae11bb7e32cc034be95d622b5c4768d4c9
KNR1997 <kethakaranasinghe@gmail.com>
2025-10-19 12:14:02 +0530

    feat: auth user model changed to accounts.user

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,3 +142,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_CREDENTIALS = True
+
+AUTH_USER_MODEL="accounts.User"
