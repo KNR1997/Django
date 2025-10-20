ca3b3816490ddfe3107b2ba8f94366f783c0cff3
KNR1997 <kethakaranasinghe@gmail.com>
2025-10-21 01:12:23 +0530

    feat: Product urls

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('product.urls')),
     path('api/auth/', include('accounts.urls')),
+    path('api/', include('customer.urls')),
 ]
