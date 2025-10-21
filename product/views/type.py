d1cfefd9eae55b33e50b3179acd96d20b60a0243
KNR1997 <kethakaranasinghe@gmail.com>
2025-10-21 23:39:03 +0530

    feat: add permissions to views

--- product/views/type.py
+++ product/views/type.py
@@ -1,3 +1,4 @@
+from core.permissions.base import allow_permission, ROLE
 from core.views.base import BaseViewSet
 from product.models import Type
 from product.serializers import TypeSerializer
@@ -21,11 +22,14 @@ class TypeViewSet(BaseViewSet):
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
+    @allow_permission([ROLE.ADMIN])
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
+    @allow_permission([ROLE.ADMIN], creator=True, model=Type)
     def partial_update(self, request, *args, **kwargs):
         return super().partial_update(request, *args, **kwargs)
 
+    @allow_permission([ROLE.ADMIN], creator=True, model=Type)
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
