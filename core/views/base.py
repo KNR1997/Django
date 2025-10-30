012394e94c30c2d14dc9ba2fb4641713871b8b72
KNR1997 <kethakaranasinghe@gmail.com>
2025-10-30 23:50:39 +0530

    feat: BaseAPIView

--- core/views/base.py
+++ core/views/base.py
@@ -4,7 +4,9 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from core.utils.exception_logger import log_exception
 
@@ -13,6 +15,8 @@ class BaseViewSet(ModelViewSet):
     model = None
 
     filter_backends = (DjangoFilterBackend, SearchFilter)
+
+    authentication_classes = [JWTAuthentication]
 
     filterset_fields = []
 
@@ -24,6 +28,43 @@ class BaseViewSet(ModelViewSet):
         except Exception as e:
             log_exception(e)
             raise APIException("Please check the view", status.HTTP_400_BAD_REQUEST)
+
+    def handle_exception(self, exc):
+        """
+        Handle any exception that occurs, by returning an appropriate response,
+        or re-raising the error.
+        """
+        try:
+            response = super().handle_exception(exc)
+            return response
+        except Exception as e:
+            if isinstance(e, IntegrityError):
+                return Response(
+                    {"error": "The payload is not valid"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            log_exception(e)
+            return Response(
+                {"error": "Something went wrong please try again later"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class BaseAPIView(APIView):
+    permission_classes = []
+
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+
+    authentication_classes = []
+
+    filterset_fields = []
+
+    search_fields = []
+
+    def filter_queryset(self, queryset):
+        for backend in list(self.filter_backends):
+            queryset = backend().filter_queryset(self.request, queryset, self)
+        return queryset
 
     def handle_exception(self, exc):
         """
